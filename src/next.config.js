8133b8f25b750d434de26727e8e86f3bd4824c05
Vijay Sasinathan <vj@intelnut.com>
2021-03-15 19:37:13 +0530

    Web security: next-secure-headers & webpack-subresource-integrity dev dependencies. Updated nextjs config.

--- src/next.config.js
+++ src/next.config.js
@@ -1,11 +1,42 @@
 const withPWA = require('next-pwa');
 const isDev = process.env.NODE_ENV !== 'production';
 
+const SriPlugin = require("webpack-subresource-integrity");
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = withPWA({
     distDir: '../dist',
     pwa: {
         dest: 'public',
         register: true,
         disable: isDev,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.output.crossOriginLoading = "anonymous";
+        if (!isDev) {
+            config.plugins.push(
+                new SriPlugin({
+                    hashFuncNames: ["sha256", "sha384"],
+                    enabled: true,
+                })
+            );
+        }
+
+        config.plugins.push(new MomentLocalesPlugin());
+
+        // Important: return the modified config
+        return config;
+    },
+    async headers() {
+        if (!isDev) {
+            return [
+                {
+                    source: "/(.*)",
+                    headers: createSecureHeaders(),
+                },
+            ]
+        } else {
+            return []
+        }
     },
 });
