24053d7bd6e600b4d7cac92e7d96cd1fabb22773
Vijay Sasinathan <vj@intelnut.com>
2021-03-20 21:38:59 +0530

    Basic setup to test schema validation

--- src/rest/functions.js
+++ src/rest/functions.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const cors = require('cors');
 const functions = require('firebase-functions');
+const userSchema = require('./schema/user.json');
 
-const rest = express();
-rest.all('/test', (req, res) => {
-    res.json({
-        message: "Hello World"
+const {
+    Validator,
+    ValidationError,
+} = require("express-json-validator-middleware");
+
+const { validate } = new Validator();
+
+const schemaValidationErrorMiddleware = (error, request, response, next) => {
+    if (response.headersSent) {
+        return next(error);
+    }
+
+    const isValidationError = error instanceof ValidationError;
+
+    if (!isValidationError) {
+        return next(error);
+    }
+
+    response.status(400).json({
+        errors: error.validationErrors,
     });
-});
 
-exports.rest = functions.https.onRequest(rest);
+    next();
+}
+
+const restApp = express();
+
+restApp.use(cors());
+
+restApp.use(express.json());
+
+const userRouteHandler = (request, response, next) => {
+    response.json(request.body);
+    next();
+}
+
+restApp.post(
+    "/user",
+    validate({ body: userSchema }),
+    userRouteHandler
+);
+
+restApp.use(schemaValidationErrorMiddleware);
+
+exports.rest = functions.https.onRequest(restApp);
