125fbf05f53fc5240cbd61e1b0b82d726219f6e3
Vijay Sasinathan <vj@intelnut.com>
2021-03-23 01:41:47 +0530

    /login/schema

--- src/rest/routes/login/route.js
+++ src/rest/routes/login/route.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { validate: validateSchema } = require('../../middleware/schema');
 const loginSchema = require('./schema.json');
+
+router.get('/schema', (req, res) => {
+    res.status(200).json(loginSchema);
+})
