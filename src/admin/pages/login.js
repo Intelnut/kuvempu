a7f8c2142113c6fa717a2412dafa69bfe6251511
Vijay Sasinathan <vj@intelnut.com>
2021-03-25 04:21:13 +0530

    Login and Setup route

---
+++ src/admin/pages/login.js
@@ -0,0 +1,11 @@
+
+/** @format */
+
+import React from 'react';
+import Login from '../components/Login';
+
+const Block = (props) => {
+    return <Login />;
+};
+
+export default Block;
---
+++ src/admin/pages/setup.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react';
+import Setup from '../components/Setup';
+
+const Block = (props) => {
+    return <Setup />;
+};
+
+export default Block;
