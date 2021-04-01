404111f54bf495a603ebac22873423bfe1b07de6
Vijay Sasinathan <vj@intelnut.com>
2021-04-01 15:58:34 +0530

    Material-ui layout

--- src/admin/src/component/Manage/index.js
+++ src/admin/src/component/Manage/index.js
@@ -2,19 +2,24 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import React from 'react';
 
+import Layout from '../Layout';
+
 import Schema from '../Schema';
 import Settings from '../Settings';
 import Dashboard from '../Dashboard';
 
 const Component = (props) => {
+
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/:schema" component={Schema} />
-                <Route path="/settings" component={Settings} />
-                <Route path="/" component={Dashboard} />
-            </Switch>
-        </BrowserRouter>
+        <Layout>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/:schema" component={Schema} />
+                    <Route path="/settings" component={Settings} />
+                    <Route path="/" component={Dashboard} />
+                </Switch>
+            </BrowserRouter>
+        </Layout>
     )
 };
 
