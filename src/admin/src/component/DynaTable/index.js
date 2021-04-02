b49ab2fdf06e992aef9261238330a84cc667434a
Vijay Sasinathan <vj@intelnut.com>
2021-04-02 22:10:48 +0530

    Preparing for dyna table implementation

--- src/admin/src/component/DynaTable/index.js
+++ src/admin/src/component/DynaTable/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useResource } from '../../context/Resource';
+
+import { DataGrid } from '@material-ui/data-grid';
+
+import { pickBy, values } from 'lodash';
 
 const Component = (props) => {
+
+    const [loading, setLoading] = useState(true);
+    const [columns, setColumns] = useState([]);
+    const [rows, setRows] = useState([]);
+
+    const { schema, model, resource } = useResource();
+
+    useEffect(() => {
+
+        const setupTableProps = () => {
+            setLoading(false);
+        }
+
+        schema && model && setupTableProps();
+
+        return () => {
+            setLoading(true);
+            setColumns([]);
+            setRows([]);
+        }
+    }, [schema, model]);
+
     return (
-        <div>DynaTable</div>
+        <div>
+            <DataGrid loading={loading} rows={rows} columns={columns} pageSize={20} />
+        </div>
     )
 };
 
