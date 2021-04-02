7957f4f235e16cf19f276527fa29e3b29652fa56
Vijay Sasinathan <vj@intelnut.com>
2021-04-03 03:32:13 +0530

    Render data in data-grid component

--- src/admin/src/component/DynaTable/index.js
+++ src/admin/src/component/DynaTable/index.js
@@ -17,6 +17,31 @@ const Component = (props) => {
     useEffect(() => {
 
         const setupTableProps = () => {
+
+            // get columns to render
+            let allowed = pickBy(schema.properties, (value, key) => value.listView);
+            let columns = Object.keys(_columns).map((key, i) => {
+                let column = {};
+                let value = allowed[key];
+                column.field = key;
+                column.headerName = value.title;
+                return column;
+            })
+            columns.push({ field: 'id', headerName: 'ID' });
+
+            // get rows to render
+            let rows = model.map((value) => {
+                let row = {};
+                row.id = value.id;
+                Object.keys(allowed).forEach((key, i) => {
+                    row[key] = value[key];
+                });
+                return row;
+            })
+
+            setColumns(columns);
+            setRows(rows);
+
             setLoading(false);
         }
 
@@ -30,7 +55,7 @@ const Component = (props) => {
     }, [schema, model]);
 
     return (
-        <div>
+        <div style={{ height: 600 }}>
             <DataGrid loading={loading} rows={rows} columns={columns} pageSize={20} />
         </div>
     )
