12db30165b3457e6cc492e49a9b1f5ed567ffb85
Vijay Sasinathan <vj@intelnut.com>
2021-04-02 10:48:09 +0530

    Provider for Manage and Settings context

--- src/admin/src/component/Resource/index.js
+++ src/admin/src/component/Resource/index.js
@@ -2,21 +2,24 @@ import { Switch, Route } from 'react-router-dom';
 
 import React from 'react';
 
-import Layout from '../Layout';
+import ResourceProvider from '../../context/Resource';
 
+import Layout from '../Layout';
 import Manage from '../Manage';
 import Settings from '../Settings';
 import Dashboard from '../Dashboard';
 
 const Component = (props) => {
     return (
-        <Layout>
-            <Switch>
-                <Route path={`/settings`} component={Settings} />
-                <Route path={`/manage/:schema`} component={Manage} />
-                <Route path={`/`} component={Dashboard} />
-            </Switch>
-        </Layout>
+        <ResourceProvider>
+            <Layout>
+                <Switch>
+                    <Route path={`/settings`} component={Settings} />
+                    <Route path={`/manage/:schema`} component={Manage} />
+                    <Route path={`/`} component={Dashboard} />
+                </Switch>
+            </Layout>
+        </ResourceProvider>
     )
 };
 
