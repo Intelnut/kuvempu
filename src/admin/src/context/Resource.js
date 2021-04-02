673b5770fe89b929d7cf844cfb2e9783763bad32
Vijay Sasinathan <vj@intelnut.com>
2021-04-02 08:42:25 +0530

    Contracts to fetch resource schema and data

--- src/admin/src/context/Resource.js
+++ src/admin/src/context/Resource.js
@@ -26,7 +26,49 @@ export const useResource = () => {
  */
 const ResourceProvider = ({ children }) => {
 
-    return (<ResourceContext.Provider value={{}}>{children}</ ResourceContext.Provider>)
+    const [resourceType, setResourceType] = useState(null);
+    const [resourceId, setResourceId] = useState(null);
+    const [data, setData] = useState(null);
+    const [schema, setSchema] = useState(null);
+
+    // fetch schema when resource type changes
+    useEffect(() => {
+        const response = await http.get(`${resource}/schema`);
+        const schema = response.data;
+        setSchema(schema);
+    }, [resourceType]);
+
+    // fetch all resources or individual resource
+    useEffect(() => {
+
+        const requests = {
+            async all() {
+                return await http.get(`${resource}`);
+            },
+
+            async single() {
+                return http.get(`${resource}/${resourceId}`);
+            }
+        }
+
+        const request = requests[resourceId] || requests['single'];
+        const response = await request();
+        const data = response.data;
+
+        setData(data);
+
+    }, [resourceType, resourceId]);
+
+    return (
+        <ResourceContext.Provider value={{
+            data,
+            resourceType,
+            resourceId,
+            schema,
+            setResourceType,
+            setResourceId
+        }}>{children}</ ResourceContext.Provider>
+    )
 }
 
 export default ResourceProvider;
