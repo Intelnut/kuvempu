846fae4b46f26edc9a8ded0534df473812659686
Vijay Sasinathan <vj@intelnut.com>
2021-04-03 13:15:03 +0530

    Feedback

--- src/admin/src/component/DynaForm/index.js
+++ src/admin/src/component/DynaForm/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import { useResource } from '../../context/Resource';
 
+import Alert from '@material-ui/lab/Alert';
+
 import { AutoForm } from "uniforms-material";
 import Ajv from 'ajv';
 import { JSONSchemaBridge } from 'uniforms-bridge-json-schema';
@@ -18,6 +20,8 @@ const createValidator = (schema) => {
 }
 
 const Component = (props) => {
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
     const [loading, setLoading] = useState(true);
     const [schemaBridge, setSchemaBridge] = useState(null);
     const { schema, model, save } = useResource();
@@ -40,12 +44,17 @@ const Component = (props) => {
     }, [schema, model]);
 
     const handleSubmit = async (input) => {
+        setError(null);
+        setSuccess(null);
         let response = await save(input);
-        // TODO: Handle success/error
+        setError(response.error);
+        setSuccess(response.success && 'Success');
     }
 
     return (
         <div>
+            {error && <Alert variant='filled' severity='error'>{error}</Alert>}
+            {success && <Alert variant='filled' severity='success'>{success}</Alert>}
             {!loading && <AutoForm
                 schema={schemaBridge}
                 model={model}
